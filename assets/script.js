3395b1a52576bf2cf5c28a012a1de11156898fa1
Jake Norris <jacobnorris013@gmail.com>
2023-06-09 11:22:41 -0400

    added final js functions

--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,146 @@
 var timeEl = document.querySelector(".time");
-var mainEl = document.getElementById("main");
-var quizEl = document.getElementById(".quiz");
-var questionNumberEl = document.getElementById(".questionNumber");
-var questionEl = document.getElementById(".question");
-var answerEL = document.getElementById(".answer");
+var mainEl = document.querySelector(".main");
+var questionNumberEl = document.getElementById("questionNumber");
+var questionEl = document.getElementById("question");
+var answerEl = document.getElementById("answers");
+var startButton = document.getElementById("startButton");
+var nextQuestionButton = document.getElementById("nextQuestion");
+var tryAgainButton = document.getElementById("tryAgain");
 
 var secondsLeft = 75;
-//Code for timer and quiz end
-function setTime() {
-  var timerInterval = setInterval(function() {
+var currentQuestion = 0;
+var score = 0;
+var timerInterval;
+
+var quiz = [
+  {
+    questionNumber: "Question 1",
+    question: "What is the capital of France?",
+    answers: ["Paris", "London", "Berlin", "Rome"],
+    correctAnswerIndex: 0
+  },
+  {
+    questionNumber: "Question 2",
+    question: "Who is the author of 'To Kill a Mockingbird'?",
+    answers: ["Harper Lee", "J.K. Rowling", "F. Scott Fitzgerald", "George Orwell"],
+    correctAnswerIndex: 0
+  },
+  // Add more questions...
+];
+
+function startTimer() {
+  timerInterval = setInterval(function () {
     secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+    timeEl.textContent = secondsLeft + " seconds left";
 
-    if(secondsLeft === 0) {
+    if (secondsLeft <= 0) {
       clearInterval(timerInterval);
-      sendMessage();
+      endQuiz();
     }
-
   }, 1000);
 }
 
-function sendMessage() {
-  timeEl.textContent = " ";
-  var imgEl = document.createElement("img");
-  imgEl.setAttribute("src", "images/image_1.jpg");
-  mainEl.appendChild(imgEl);
-
+function startQuiz() {
+  startButton.style.display = "none";
+  nextQuestionButton.style.display = "block";
+  tryAgainButton.style.display = "none";
+  secondsLeft = 75;
+  score = 0;
+  currentQuestion = 0;
+  startTimer();
+  showQuestion();
+  var highScoreButton = document.getElementById("high-score");
+  highScoreButton.addEventListener("click", showHighScores);
 }
 
-setTime();
+function showQuestion() {
+  var quizQuestion = quiz[currentQuestion];
 
+  questionNumberEl.textContent = quizQuestion.questionNumber;
+  questionEl.textContent = quizQuestion.question;
+  answerEl.innerHTML = "";
 
-//Code for quiz questions and answer
+  for (var i = 0; i < quizQuestion.answers.length; i++) {
+    var answer = quizQuestion.answers[i];
+    var answerButton = document.createElement("button");
+    answerButton.textContent = answer;
+    answerButton.addEventListener("click", function () {
+      checkAnswer(this, quizQuestion.correctAnswerIndex);
+    });
 
-
-var quiz = [
-    {
-      questionNumber: "Question 1",
-      question: "What is the capital of France?",
-      answers: ["London", "Paris", "Madrid", "Berlin"],
-      correctAnswerIndex: 1
-    },
-    {
-      questionNumber: "Question 2",
-      question: "What is the largest country in the world by land area?",
-      answers: ["Russia", "China", "Canada", "United States"],
-      correctAnswerIndex: 0
-    },
-    // add more questions here
-];
-
-function question(index) {
-    var question = quiz[index];
-    var questionNumberEl = document.querySelector(".questionNumber");
-    var questionEl = document.querySelector(".question");
-    var answerEls = document.querySelectorAll(".answers li");
-  
-    // populate the elements
-    questionNumberEl.textContent = question.questionNumber;
-    questionEl.textContent = question.question;
-    for (var i = 0; i < question.answers.length; i++) {
-      answerEls[i].textContent = question.answers[i];
-    }
+    var li = document.createElement("li");
+    li.appendChild(answerButton);
+    answerEl.appendChild(li);
+  }
 }
 
-var currentQuestionIndex = 0;
-question(currentQuestionIndex);
+function checkAnswer(selectedAnswerButton, correctAnswerIndex) {
+  if (correctAnswerIndex === Array.from(answerEl.children).indexOf(selectedAnswerButton.parentElement)) {
+    alert("Correct!");
+    score++;
+  } else {
+    alert("Wrong!");
+    secondsLeft -= 10;
+  }
 
-var nextQuestionButton = document.querySelector("#nextQuestion");
-nextQuestionButton.addEventListener("click", function() {
-  currentQuestionIndex++;
-  question(currentQuestionIndex);
+  currentQuestion++;
+
+  if (currentQuestion < quiz.length) {
+    showQuestion();
+  } else {
+    endQuiz();
+  }
+}
+
+function endQuiz() {
+  clearInterval(timerInterval);
+  timeEl.textContent = "Quiz Over!";
+  questionNumberEl.textContent = "Game Over!";
+  questionEl.textContent = "Final Score: " + score;
+  answerEl.innerHTML = "";
+  startButton.style.display = "none";
+  nextQuestionButton.style.display = "none";
+  tryAgainButton.style.display = "block";
+
+  var playerName = prompt("Enter your name:");
+  if (playerName) {
+    var playerScore = {
+      name: playerName,
+      score: score
+    };
+    var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+    highScores.push(playerScore);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+  }
+}
+
+function resetQuiz() {
+  questionNumberEl.textContent = "";
+  questionEl.textContent = "";
+  answerEl.innerHTML = "";
+  startButton.style.display = "block";
+  nextQuestionButton.style.display = "none";
+  tryAgainButton.style.display = "none";
+}
+
+function showHighScores() {
+  var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+  var highScoreList = document.getElementById("high-score-list");
+  highScoreList.innerHTML = "";
+
+  for (var i = 0; i < highScores.length; i++) {
+    var scoreEntry = document.createElement("p");
+    scoreEntry.textContent = highScores[i].name + " - " + highScores[i].score;
+    highScoreList.appendChild(scoreEntry);
+  }
+}
+
+
+
+startButton.addEventListener("click", startQuiz);
+nextQuestionButton.addEventListener("click", function () {
+  currentQuestion++;
+  showQuestion();
 });
+tryAgainButton.addEventListener("click", resetQuiz);
